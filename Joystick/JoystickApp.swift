3ec2ad941345872def736145a196d9cdcdfb546c
minjae Kim <hjn966@naver.com>
2023-11-17 15:37:50 +0900

    Initial Commit

---
+++ Joystick/JoystickApp.swift
@@ -0,0 +1,17 @@
+//
+//  JoystickApp.swift
+//  Joystick
+//
+//  Created by 김민재 on 11/17/23.
+//
+
+import SwiftUI
+
+@main
+struct JoystickApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
